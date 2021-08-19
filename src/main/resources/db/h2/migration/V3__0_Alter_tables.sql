9755dca7d5329de9bf45bfe45529118dfe2cc858
Alena Salanevich <alena_salanevich@epam.com>
2021-08-19 21:53:02 +0200

    added integration with liquibase

--- src/main/resources/db/h2/migration/V3__0_Alter_tables.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE employees ADD CONSTRAINT fk_employees FOREIGN KEY (id) REFERENCES employees (id);
-ALTER TABLE companies ADD CONSTRAINT fk_companies FOREIGN KEY (id) REFERENCES companies (id);
-CREATE INDEX company_name ON companies (address);
